6788c5cfa7e3f56c890516b758a48fa30caacf14
johan855 <johan855@gmail.com>
2019-02-26 22:24:40 +0100

    Comment out psql connect command

--- setup_db.sh
+++ setup_db.sh
@@ -6,5 +6,15 @@ docker run --rm --name pg-docker \
            -d -p 127.0.0.1:5432:5432 \
            -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
 
+#install phppg_admin
+docker -exec -ti postgres /bin/bash
+createuser testuser -P --createdb -h postgres1 -U postgres
+##->add password creation
+
+
+docker run --name phppgadmin-docker -ti -d \
+           -p 8080:80 \
+           -e DB_HOST=postgres1 phppgadmin
+
 #To connect to PG without installing it on the current instance, run:
 #docker exec --tty --interactive pg-docker psql -h localhost -U postgres -d postgres
