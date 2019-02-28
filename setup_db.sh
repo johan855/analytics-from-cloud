82ed9c1b51b25098035d5801dc58b7340151b8cf
johan855 <johan855@gmail.com>
2019-02-28 23:36:30 +0100

    Added user creation to postgres setup

--- setup_db.sh
+++ setup_db.sh
@@ -6,15 +6,19 @@ docker run --rm --name pg-docker \
            -d -p 127.0.0.1:5432:5432 \
            -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
 
-#install phppg_admin
-docker -exec -ti postgres /bin/bash
-createuser testuser -P --createdb -h postgres1 -U postgres
-##->add password creation
-
-
-docker run --name phppgadmin-docker -ti -d \
-           -p 8080:80 \
-           -e DB_HOST=postgres1 phppgadmin
-
 #To connect to PG without installing it on the current instance, run:
 #docker exec --tty --interactive pg-docker psql -h localhost -U postgres -d postgres
+
+#Create login user and install phppg_admin
+docker exec --tty --interactive pg-docker /bin/bash
+su -c "psql -c \"CREATE USER johan855 WITH LOGIN PASSWORD 'password';\"" postgres
+
+#Test the following:
+https://hub.docker.com/r/dockage/phppgadmin/
+https://hub.docker.com/r/zhajor/docker-phppgadmin/
+https://hub.docker.com/r/keepitcool/phppgadmin/
+
+docker run --rm --name phppgadmin-docker -ti -d \
+           -p 8080:80 \
+           -e PHP_PG_ADMIN_SERVER_HOST=localhost dockage/phppgadmin:latest
+
