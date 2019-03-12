bc5a47207d23c3907da0fcb4f55f46098c229cc1
johan855 <johan855@gmail.com>
2019-03-12 21:46:34 +0100

    added AND RUN commands

--- setup_prophet.sh
+++ setup_prophet.sh
@@ -1,7 +1,7 @@
 FROM ubuntu:18.04
 RUN sudo apt-get update \
-    sudo apt install python3-pip -y \
-    pip3 install numpy \
-    pip3 install cython \
-    pip3 install pystan \
-    pip3 install fbprophet
+    && sudo apt install python3-pip -y \
+    && pip3 install numpy \
+    && pip3 install cython \
+    && pip3 install pystan \
+    && pip3 install fbprophet
