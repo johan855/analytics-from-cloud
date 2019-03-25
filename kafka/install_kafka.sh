5e01572464f0660cab3af885fda0bdc03db9648f
johan855 <johan855@gmail.com>
2019-03-25 23:36:57 +0100

    Finished kafka auto setup with 2 brokers

--- kafka/install_kafka.sh
+++ kafka/install_kafka.sh
@@ -1,4 +1,3 @@
-
 # Install confluent-kafka in python
 pip install confluent-kafka
 
@@ -15,35 +14,35 @@ echo "export PATH=/home/etl_montredo/kafka_2.12-2.1.1/bin:$PATH" >> .bashrc
 # Create data folders and modify property config (log folders and ports)
 mkdir ~/kafka_2.12-2.1.1/data
 mkdir ~/kafka_2.12-2.1.1/data/zookeeper
+mkdir ~/kafka_2.12-2.1.1/data/kafka0
 mkdir ~/kafka_2.12-2.1.1/data/kafka1
-mkdir ~/kafka_2.12-2.1.1/data/kafka2
 
 
 # Modify log folders
 sed -i -e '/dataDir/ s/\/tmp\/zookeeper/~\/kafka_2.12-2.1.1\/data\/zookeeper/' \
     kafka_2.12-2.1.1/config/zookeeper.properties
+sed -i -e '/dataDir/ s/\/tmp\/kafka-logs/~\/kafka_2.12-2.1.1\/data\/kafka0/' \
+    kafka_2.12-2.1.1/config/server.0.properties
 sed -i -e '/dataDir/ s/\/tmp\/kafka-logs/~\/kafka_2.12-2.1.1\/data\/kafka1/' \
     kafka_2.12-2.1.1/config/server.1.properties
-sed -i -e '/dataDir/ s/\/tmp\/kafka-logs/~\/kafka_2.12-2.1.1\/data\/kafka2/' \
-    kafka_2.12-2.1.1/config/server.2.properties
 
-# Modify broker.id
+
+# Create Kafka broker config files (2 brokers)
+cp kafka_2.12-2.1.1/config/server.properties kafka_2.12-2.1.1/config/server.0.properties
+cp kafka_2.12-2.1.1/config/server.properties kafka_2.12-2.1.1/config/server.1.properties
+
+# Modify broker.id in config files
 sed -i -e '/broker.id/ s/0/1/' kafka_2.12-2.1.1/config/server.1.properties
-sed -i -e '/broker.id/ s/0/2/' kafka_2.12-2.1.1/config/server.2.properties
 
-# Modify listeners
-sed -i '/test/s/^#//g' file
-sed -i -e '/listeners/ s/^#/' kafka_2.12-2.1.1/config/server.1.properties
-sed -i -e '/listeners/ s/^#/' kafka_2.12-2.1.1/config/server.2.properties
-
+# Modify listeners in config files
+sed -i -e '/listeners=PLAINTEXT:\/\/:9092/ s/^#//' kafka_2.12-2.1.1/config/server.0.properties
+sed -i -e '/listeners=PLAINTEXT:\/\/:9092/ s/^#//' kafka_2.12-2.1.1/config/server.1.properties
+sed -i -e '/listeners/ s/PLAINTEXT:\/\/:9092/PLAINTEXT:\/\/:9093/' kafka_2.12-2.1.1/config/server.1.properties
 
 
 # Initiallize Zookeeper
 nohup zookeeper-server-start.sh kafka_2.12-2.1.1/config/zookeeper.properties &
 
 # Start 2 Kafka brokers (General setup for 3 partitions, 2 brokers and rep. factor of 2 per topic)
-nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.1.properties &
-nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.2.properties &
-
-
-TODO: Set sed to uncomment nth occurrence of the listener parameter
+nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.0.properties &
+nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.1.properties
