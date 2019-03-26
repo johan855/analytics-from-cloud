98e04b1e6efb1913a4ac4166f3cdf8d025f74f0e
johan855 <johan855@gmail.com>
2019-03-27 00:00:35 +0100

    Finished kafka auto setup with 2 brokers

--- kafka/install_kafka.sh
+++ kafka/install_kafka.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Install confluent-kafka in python
 pip install confluent-kafka
 
@@ -42,7 +44,12 @@ sed -i -e '/listeners/ s/PLAINTEXT:\/\/:9092/PLAINTEXT:\/\/:9093/' kafka_2.12-2.
 
 # Initiallize Zookeeper
 nohup zookeeper-server-start.sh kafka_2.12-2.1.1/config/zookeeper.properties &
-
+sleep 5
 # Start 2 Kafka brokers (General setup for 3 partitions, 2 brokers and rep. factor of 2 per topic)
 nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.0.properties &
+sleep 5
 nohup kafka-server-start.sh kafka_2.12-2.1.1/config/server.1.properties
+
+
+# Create first topic (3 partitions and 2 as rep. factor)
+kafka-topics.sh --zookeeper localhost:2181 --topic first_topic --create --partitions 3 --replication-factor 2
