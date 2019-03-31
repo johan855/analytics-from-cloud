1220690e5efe27b3718a134e83e79b199ef98b07
johan855 <johan855@gmail.com>
2019-03-31 01:45:19 +0100

    Setup pre-docker instructions to setup airflow quickly

--- etl/setup_airflow.sh
+++ etl/setup_airflow.sh
@@ -1,3 +1,25 @@
+# airflow needs a home, ~/airflow is the default,
+export AIRFLOW_HOME=~/airflow
+export SLUGIFY_USES_TEXT_UNIDECODE=yes
+pip3 install apache-airflow
+
+# initialize the database
+airflow initdb
+
+# start the web server, default port is 8080
+airflow webserver -p 8080
+
+# start the scheduler
+airflow scheduler
+
+---------------------------------
+#Run Airflow container
+docker pull puckel/docker-airflow
+
+#Sequential executor:
+docker run -d -p 8080:8080 -e LOAD_EX=y puckel/docker-airflow webserver
+
+---------------------------------
 #Run Airflow container
 docker pull puckel/docker-airflow
 
